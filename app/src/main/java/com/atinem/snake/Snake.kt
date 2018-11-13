d6150cf71f04645f1eb3092e7e20e31e6179680b
Aula4Ignacio <ignacio@institutotame.com>
2018-11-13 13:50:17 +0100

    Adding snake body and head bitmaps

--- app/src/main/java/com/atinem/snake/Snake.kt
+++ app/src/main/java/com/atinem/snake/Snake.kt
@@ -1,18 +1,31 @@
 package com.atinem.snake
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Point
 
-class Snake {
+class Snake(context : Context, mr : Point, ss : Int) {
 
-    private val segmentLocations : List<Point>
+    private val segmentLocations = mutableListOf<Point>()
 
-    private mSegmentSize : Int
+    private val mSegmentSize = ss
 
-    private mMoveRange : Point
+    private val mMoveRange = mr
 
-    private halfWayPoint : Int
+    private val halfWayPoint = mr.x * ss /2
 
     private enum class Heading {
         UP, RIGHT, DOWN, LEFT
     }
+
+    private val heading = Heading.RIGHT
+
+    private val mBitmapHeadRight : Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.head)
+    private val mBitmapHeadLeft : Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.head)
+    private val mBitmapHeadUp : Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.head)
+    private val mBitmapHeadDown : Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.head)
+
+    private val mBitmapBody : Bitmap = BitmapFactory.decodeResource(context.resources, R.drawable.body)
+
 }
